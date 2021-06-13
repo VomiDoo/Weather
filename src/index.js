de56d8fae024bb93e73704fc532deac1237d067b
VomiDoo <didi_doo@mail.ru>
2021-06-13 12:51:50 +0300

    feat: install webpack and create dist

--- src/index.js
+++ src/index.js
@@ -1,19 +1,8 @@
 const test = /[0-9]/g
 // const _ = require('lodash');
 
-// const { printWeather } = require ('./printWeather')
+const { printWeather } = require ('./printWeather')
 
-function printWeather (data) {
-    document.querySelector('.main__title').innerHTML = data.location
-    document.querySelector('.main__temperature').innerHTML = `${data.temperature} &#8451;`
-    document.querySelector('.main__img').style.backgroundImage = `url(${data.icon})`
-    document.querySelector('.inform__time').innerHTML = `Time: ${data.time}`
-    document.querySelector('.inform__pressure').innerHTML = `Pressure: ${data.pressure} MB`
-    document.querySelector('.inform__feelslike').innerHTML = `Feels like: ${data.feelslike} &#8451;`
-    document.querySelector('.inform__weather').innerHTML = `Today is ${data.discription}`
-    document.querySelector('.wind-direction').innerHTML = `Wind: ${data.windDir}`
-    document.querySelector('.wind-speed').innerHTML = `Speed: ${data.windSpeed} km/h`
-}
 
 function HistoryObject (data) {
     this.location = `${data.location.country}, ${data.location.region}`;
