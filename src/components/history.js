315ffc78c33216afe7c1883f6de8a3c82d9d9f16
VomiDoo <didi_doo@mail.ru>
2021-06-20 17:18:38 +0300

    fix: js files

--- src/components/history.js
+++ src/components/history.js
@@ -1,36 +1,30 @@
 const _ = require('lodash');
 
-document.querySelector('.history__close-btn').addEventListener('click', () => {
-  document.querySelector('.history').classList.remove('history__open');
-  document.querySelector('.history__wrap').classList.remove('history__open');
-  document.querySelector('.history__ul').innerHTML = '';
-});
-let history = [];
+const prepareModule = () => {
+  document.querySelector('.history__close-btn').addEventListener('click', () => {
+    document.querySelector('.history').classList.remove('history__open')
+    document.querySelector('.history__wrap').classList.remove('history__open')
+    document.querySelector('.history__ul').innerHTML = ''
+  })
+  return localStorage.getItem('history') ? JSON.parse(localStorage.getItem('history')) : [];
+};
 
-export function setLocal(weather) {
-  history.unshift(weather);
-  history = _.uniqBy(history, (o) => o.location);
-  history = _.take(history, 5);
-  localStorage.setItem('history', JSON.stringify(history));
-}
+let history = prepareModule();
 
-function getLocal() {
-  if (localStorage.getItem('history')) {
-    history = JSON.parse(localStorage.getItem('history'));
-  }
-}
-getLocal();
+export const setLocal = (weather) => {
+    history.unshift(weather);
+    history = _.uniqBy(history, (e) => e.location)
+	history = _.take(history, 5)
+	localStorage.setItem('history', JSON.stringify(history))
+};
 
-export function printHistory() {
-  _.forEach(history, (i) => {
-    document.querySelector('.history__ul').insertAdjacentHTML(
-      'beforeend',
-      `<li class="history__li">
+export const printHistory = () => {
+    _.forEach(history, i => {
+        document.querySelector('.history__ul').insertAdjacentHTML('beforeend',`<li class="history__li">
                                                                                 <p class="history__name border">${i.location}</p>
                                                                                 <p class="history__temperature border">${i.temperature} &#8451;</p>
                                                                                 <p class="history__weather border"><img src="${i.icon}" class="history__weather-icon"></p>
                                                                                 <p class="history__wind border">${i.windSpeed} km/s ${i.windDir}</p>
-                                                                            </li>`
-    );
-  });
+                                                                            </li>`)
+    })
 }
--- src/index.js
+++ src/index.js
@@ -1,32 +1,35 @@
-import { printWeather } from './components/printComponents';
+import { printWeather } from './components/printWeather';
 import { setLocal, printHistory } from './components/history';
 import './style.css';
 
 const test = /[0-9]/g;
 const _ = require('lodash');
 
-function HistoryObject(data) {
-  this.location = `${data.location.country}, ${data.location.region}`;
-  this.temperature = data.current.temperature;
-  this.windDir = data.current.wind_dir;
-  this.windSpeed = data.current.wind_speed;
+const getHistoryObject = (data) => {
   const icon = data.current.weather_icons[0];
-  this.icon = icon;
-  this.time = data.location.localtime.slice(11, 16);
-  this.pressure = data.current.pressure;
-  this.feelslike = data.current.feelslike;
-  this.discription = data.current.weather_descriptions;
-}
+  
+  return {
+    location: `${data.location.country}, ${data.location.region}`,
+    temperature: data.current.temperature,
+    windDir: data.current.wind_dir,
+    windSpeed: data.current.wind_speed,
+    icon,
+    time: data.location.localtime.slice(11, 16),
+    pressure: data.current.pressure,
+    feelslike: data.current.feelslike,
+    discription: data.current.weather_descriptions,
+  }
+};
 
 const fetchAPI = (coord) => {
-  const city = document.querySelector('.header__input--city').value || coord;
+  const city = document.querySelector('.header__input--city').value;
   if (coord || !city.match(test)) {
     fetch(
-      `http://api.weatherstack.com/current?access_key=bdeec7a4beb014daa4415d15b0447360&query=${city}`
+      `http://api.weatherstack.com/current?access_key=bdeec7a4beb014daa4415d15b0447360&query=${city || coord}`
     )
       .then((data) => data.json())
       .then((data) => {
-        const town = new HistoryObject(data);
+        const town = getHistoryObject(data);
         printWeather(town);
         setLocal(town);
       });
@@ -36,14 +39,11 @@ const fetchAPI = (coord) => {
 document.querySelector('.header').addEventListener('click', (e) => {
   if (e.target === document.querySelector('.my-weather__btn')) {
     navigator.geolocation.getCurrentPosition((position) => {
-      const coords = _.join(
-        [
+      const coords = [
           `${position.coords.latitude.toFixed(
             4
           )},${position.coords.longitude.toFixed(4)}`,
-        ],
-        ','
-      );
+        ].join(',');
       fetchAPI(coords);
     });
   }
@@ -56,6 +56,11 @@ document.querySelector('.header').addEventListener('click', (e) => {
     document.querySelector('.history').classList.add('history__open');
     document.querySelector('.history__wrap').classList.add('history__open');
     printHistory();
+  }
+
+  if (e.target === document.querySelector('.open-notification-btn')) {
+    localStorage.removeItem('disable');
+    document.querySelector('.notification').style.display = 'block';
   }
   if (e.target === document.querySelector('.clear-local-btn')) {
     localStorage.removeItem('history');
