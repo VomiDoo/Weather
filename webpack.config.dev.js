44868cf60736ee75e577e63e067468f56ba624dd
VomiDoo <didi_doo@mail.ru>
2021-06-26 11:43:29 +0300

    fix: fix webpack.config.dev

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,33 +1,8 @@
-<<<<<<< HEAD
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-=======
->>>>>>> fa2586ab2fdc6523aff49d1952091c5653054964
 const path = require('path');
 const { merge } = require('webpack-merge');
 const base = require('./webpack.config.base');
 
 const dev = merge(base, {
-<<<<<<< HEAD
-    mode: 'development',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'distDev')
-    },
-    devServer: {
-        contentBase: path.join(__dirname, 'distDev'),
-        compress: true,
-        port: 9000,
-    },
-    plugins: [
-        new HtmlWebpackPlugin({
-            title: 'Weather',
-            template: './src/index.html',
-        })
-    ],
-})
-
-module.exports = dev
-=======
   mode: 'development',
   devServer: {
     contentBase: path.join(__dirname, 'dist'),
@@ -37,4 +12,3 @@ module.exports = dev
 });
 
 module.exports = dev;
->>>>>>> fa2586ab2fdc6523aff49d1952091c5653054964
